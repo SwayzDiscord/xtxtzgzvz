836d12fc553b1bd5c0af65396b0c15bbcb9c4ede
CxtrollerFN <77362066+CxtrollerFN@users.noreply.github.com>
2021-05-25 00:16:59 -0400

    Update member-count.js

--- src/commands/member-count.js
+++ src/commands/member-count.js
@@ -1,1 +1,18 @@
 
+const { MessageEmbed } = require('discord.js');
+ 
+module.exports = {
+    name: 'member-count',
+    description: 'Shows you the member count of the server!',
+ 
+    async execute(message, args) {
+ 
+        const embed = new MessageEmbed()
+        .setTitle('Member Count')
+        .setDescription(`We currently have \`${message.guild.memberCount}\` Members in the server!`)
+        .setColor('RANDOM')
+        .setTimestamp();
+ 
+        message.channel.send(embed);
+    }
+}
